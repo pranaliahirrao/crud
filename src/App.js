b14b8ceb39f044a27130de5f20a0709280f515ef
pranaliahirrao <pranali.21810302@viit.ac.in>
2024-02-02 22:59:36 +0530

    react crud operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,188 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { EmployeeData } from "./EmployeeData";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [age, setAge] = useState();
+  const [id, setId] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
+
+  const handleEdit = (id) => {
+    const dt = data.filter((emp) => emp.id === id);
+    if (dt !== undefined) {
+      setIsUpdate(true);
+      setId(id);
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  };
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure, you want to delete this data?")) {
+        const dt = data.filter((item) => item.id !== id);
+        setData(dt);
+      }
+    }
+  };
+
+  const handleSave = (e) => {
+    let error = "";
+
+    if (firstName === "") {
+      error += "First Name is required, ";
+    }
+    if (lastName === "") {
+      error += "Last Name is required, ";
+    }
+    if (age === "" || isNaN(age)) {
+      error += "Age is required and must be a valid number.";
+    }
+
+    if (error === '') {
+      e.preventDefault();
+      const dt = [...data];
+      const newObject = {
+        id: EmployeeData.length + 1,
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+      };
+      dt.push(newObject);
+
+      setData(dt);
+      handleClear();
+    } else {
+      alert(error);
+    }
+  };
+
+  const handleUpdate = () => {
+    const index = data
+      .map((emp) => {
+        return emp.id;
+      })
+      .indexOf(id);
+
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+
+    setData(dt);
+    handleClear();
+  };
+
+  const handleClear = () => {
+    setIsUpdate(false);
+    setId();
+    setFirstName("");
+    setLastName("");
+    setAge("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "30px" }}
+      >
+        <div style={{ padding: "10px" }}>
+          <label>
+            First Name:
+            <input
+              type="text"
+              placeholder="Enter First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+            />
+          </label>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              placeholder="Enter Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+            />
+          </label>
+        </div>
+        <div style={{ padding: "10px" }}>
+          <label>
+            Age:
+            <input
+              type="number"
+              placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
+            />
+          </label>
+        </div>
+        <div style={{ padding: "10px" }}>
+          {!isUpdate ? (
+            <button className="btn btn-primary" onClick={(e) => handleSave(e)}>
+              Save
+            </button>
+          ) : (
+            <button className="btn btn-primary" onClick={() => handleUpdate()}>
+              Update
+            </button>
+          )}
+          <button className="btn btn-danger" onClick={() => handleClear()}>
+            Clear
+          </button>
+        </div>
+      </div>
+
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Sr.No</th>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((emp, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{emp.id}</td>
+                <td>{emp.firstName}</td>
+                <td>{emp.lastName}</td>
+                <td>{emp.age}</td>
+                <td>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleEdit(emp.id)}
+                  >
+                    Edit
+                  </button>
+                  &nbsp;
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(emp.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/EmployeeData.js
@@ -0,0 +1,23 @@
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: "Pranali" ,
+        lastName: "Ahirrao" ,
+        age: 20
+    },
+    {
+        id: 2,
+        firstName: "Renu" ,
+        lastName: "Patil" ,
+        age: 50
+    },
+    {
+        id: 3,
+        firstName: "User" ,
+        lastName: "Custom" ,
+        age: 26
+    }
+]
+
+
+
